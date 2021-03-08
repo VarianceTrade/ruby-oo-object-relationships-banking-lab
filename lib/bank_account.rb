689ff3b71335de3d7ea64334f69ec840df3ce583
Brandon <sugb73@gmail.com>
2021-03-07 19:14:06 -0500

    commit tree 1fac1ce3ea2c73765b5daa4c6e64d16982b92b59

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,51 @@
 class BankAccount
 
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @balance = 1000
+        @status = "open"
+    end
+
+    def name
+        @name
+    end
+
+    def balance
+        @balance
+    end
+
+    def balance=(balance)
+        @balance = balance
+    end
+
+    def status
+        @status
+    end
+
+    def status=(status)
+        @status = status
+    end
+
+    def deposit(amount)
+        @balance = @balance + amount
+    end
+
+    def display_balance
+        return "Your balance is $#{balance}."
+    end
+
+    def valid?
+        if @status == "open" && @balance > 0
+            return true
+        else
+            false
+        end
+    end
+
+    def close_account
+        @status = "closed"
+    end
+
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,64 @@
 class Transfer
   # your code here
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+  end
+
+  def sender
+    @sender
+  end 
+
+  def receiver
+    @receiver
+  end
+
+  def status
+    @status
+  end
+
+  def amount
+    @amount
+  end
+
+  def valid?
+    # how do I call on valid?
+    if @sender.valid? && @receiver.valid?
+      return true
+    else
+    end
+  end
+
+
+  def execute_transaction
+    if @status == "complete" # if transfer was already done before
+      return # don't do it again
+    end
+
+    if @sender.balance < @amount || !@sender.valid? || !@receiver.valid?
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    end
+
+    # remove amount from sender
+    @sender.balance = @sender.balance - @amount
+
+    # add amount to receiver
+    @receiver.deposit(@amount)
+
+    # set status to complete
+    @status = "complete"
+  end
+
+  def reverse_transfer
+    # write the code that will take money from the receiver and send it ot the sender
+    if @status == "complete"
+      @receiver.balance = @receiver.balance - @amount
+      @sender.balance = @sender.balance + @amount
+      @status = "reversed"
+    end
+  end
+
 end
